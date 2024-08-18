a93d828501ed5fff82a4b50f8741b92b3f80a84d
rnb-project <woogeun_tak@rnbsoft.com>
2024-08-18 13:30:08 +0900

    RNB_DressCode_Project

---
+++ dress_code/src/main/java/com/rnbsoft/dress_code/DressCodeApplication.java
@@ -0,0 +1,13 @@
+package com.rnbsoft.dress_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressCodeApplication.class, args);
+	}
+
+}
---
+++ dress_code/src/test/java/com/rnbsoft/dress_code/DressCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnbsoft.dress_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
