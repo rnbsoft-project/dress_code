2504f2edc6437524130669ea89835ad5bf434fb2
BZW <woogeun_tak@rnbsoft.com>
2024-08-27 16:27:20 +0900

    DRESS_CODE 상품DTO 추가

---
+++ dress_code/src/main/java/com/rnbsoft/dress_code/dto/productDTO.java
@@ -0,0 +1,21 @@
+package com.rnbsoft.dress_code.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class productDTO {
+
+	private String prodNum; //상품번호
+	private String prodName; //상품이름
+	private String prodCatgr; //상품카테고리
+
+}
