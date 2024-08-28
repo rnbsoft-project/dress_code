d0b26474e729f170a6e53a28e9d0992b13c98053
BZW <woogeun_tak@rnbsoft.com>
2024-08-28 11:14:38 +0900

    테스트입니다 섭비스 추가

---
+++ dress_code/src/main/java/com/rnbsoft/dress_code/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.rnbsoft.dress_code.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Transactional
+public class ProductService {
+
+}
